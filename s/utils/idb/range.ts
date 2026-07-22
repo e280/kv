a1df32b66111871cc953297587ccfba3513a99fa
Chase Moskal <chasemoskal@gmail.com>
2026-07-22 03:22:21 -0700

    tweak: use happy instead of is.happy

--- s/utils/idb/range.ts
+++ s/utils/idb/range.ts
@@ -1,10 +1,10 @@
 
-import {is} from "@e280/stz"
+import {happy} from "@e280/stz"
 import {Scan} from "../../types.js"
 
 export function idbRange({start, end}: Scan) {
-	if (is.happy(start) && is.happy(end)) return IDBKeyRange.bound(start, end, false, true)
-	if (is.happy(start)) return IDBKeyRange.lowerBound(start, false)
-	if (is.happy(end)) return IDBKeyRange.upperBound(end, true)
+	if (happy(start) && happy(end)) return IDBKeyRange.bound(start, end, false, true)
+	if (happy(start)) return IDBKeyRange.lowerBound(start, false)
+	if (happy(end)) return IDBKeyRange.upperBound(end, true)
 }
 
